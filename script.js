a1d27d06de513edcd809b3858dafbaa5b277d5b7
Robert <robertkoor0@gmail.com>
2022-08-08 18:01:26 +0300

    XO working correctly

--- script.js
+++ script.js
@@ -1,55 +1,87 @@
+// Gameboard object
+'use strict'
 const GameBoard = (function(){
     let board = [
-                 ["X", null, "X"],
-                 [null, "O", null],
+                 [null, null, null],
+                 [null, null, null],
                  [null, null, null]
                 ];
     const showBoard = () => console.log(board);
+    
+    const modify = (XO, col, row) => board[col][row]= XO;
 
-    return {showBoard, board}
+    
+    
+
+    
+    return {showBoard, board, modify}
 }
 
 )()
 
-const GameController = (function(){
-    const display =  document.querySelector('.board');
-    function render(board){
+// Player object
+const Player = (XO) => {
+    const side = XO;
+    const whichSide = () => console.log(XO);
+    return {whichSide, side}
+}
+
+GameBoard.showBoard()
+
+const player1 = Player("X")
+const player2 = Player("O")
+let activePlayer = player1;
+
+
+
+// Game controller object
+const Game = (function(){
+    function renderBoard(){
+        const display =  document.querySelector('.board');
+        
         for(let i=0;i<3;i++){
             for(let j=0;j<3;j++){
-
                 const div = document.createElement('div');
-                
-
-                if (board[i][j] == "X"){
-                    const X = document.createElement('p')
-                    X.textContent = "X"
-                    div.appendChild(X)
-                } else if (board[i][j]=="O"){
-                    const O = document.createElement('p')
-                    O.textContent = "O"
-                    div.appendChild(O)
-                }
+                div.classList.add('tile');
+                div.setAttribute('id', `${i} ${j}`);
                 display.appendChild(div);
             }
         }
     };
-    return {render}
+
+    function renderNew(col, row){
+        const id =  document.getElementById(String(col)+' '+String(row));
+        const p = document.createElement('p');
+        p.textContent =  activePlayer.side;
+        id.appendChild(p);
+    };
+
+    return {renderNew, renderBoard}
 })()
 
-const Player = (XO) => {
-    const whichSide = () => console.log(XO);
+Game.renderBoard()
 
-    return {whichSide}
+function playRound(e){
+    let col = parseInt(Array(e.target.id.split(" "))[0][0])
+    let row = parseInt(Array(e.target.id.split(" "))[0][1])
+    console.log(activePlayer.side)
 
-}
+    if(!isNaN(row) && !isNaN(col)){
+        GameBoard.modify(activePlayer.side, col, row)
+        Game.renderNew(col, row)
 
+        if (activePlayer === player1){
+            activePlayer = player2
+        } else {
+            activePlayer = player1
+        }
+        
+        console.log(GameBoard.board)
+    }
+};
 
-GameController.render(GameBoard.board)
-
-GameBoard.showBoard()
-
-let player1 = Player("X")
-let player2 = Player("O")
-
-player1.whichSide()
-player2.whichSide()
+// we get the divs what are clicked
+const tiles = document.querySelectorAll('.tile');
+tiles.forEach(tile=> {
+    tile.addEventListener('click', e => playRound(e));
+})
